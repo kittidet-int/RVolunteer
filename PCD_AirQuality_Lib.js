7331b302db84083afae196f7db895f738f0bb415
Kittidet Intharaksa <10433727+kittidet-int@users.noreply.github.com>
2026-02-14 09:43:34 +0700

    Update PCD_AirQuality_Lib.js
    Add license description

--- PCD_AirQuality_Lib.js
+++ PCD_AirQuality_Lib.js
@@ -1,3 +1,19 @@
+/**
+ * Program: PCD AirQuality Library
+ * Description: 
+ * * Copyright (C) 2026  Kittidet Intharaksa
+ * * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 function importJSONtoSheet() {
   // 1. API
   var url = "http://air4thai.pcd.go.th/services/getNewAQI_JSON.php";
